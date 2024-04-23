efae72defc98e6c2ce60715367c40e76e2f70446
Violetta Sabitova <violetsabitova@gmail.com>
2024-04-23 15:45:40 +0700

    Code v.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balance=100; //баланс телефона до пополнения
-        int amount=1100; //сумма пополнения
-        int bonus=0; //переменная для подсчета бонусов
+        int balance = 100; //баланс телефона до пополнения
+        int amount = 1100; //сумма пополнения
+        int bonus = 0; //переменная для подсчета бонусов
 
-        if (amount<1000){
+        if (amount < 1000) {
             balance = balance + amount;
         } else {
-            bonus = amount/100;
+            bonus = amount / 100;
             balance = balance + amount + bonus;
         }
 
